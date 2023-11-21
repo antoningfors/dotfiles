852634c690c6575648b827a5e2459aa500fd3cbe
Anton Ingfors <anton.ingfors@gmail.com>
2023-11-22 00:05:56 +0100

    updating bootstrap for windows to inclue nvim

--- bootstrap.bat
+++ bootstrap.bat
@@ -3,6 +3,9 @@ call git pull origin master
 call git submodule init
 call git submodule update 
 call git submodule sync 
-echo "Installing VIM Config files"
-copy /Y .vimrc C:%HOMEPATH%\
-xcopy .vim C:%HOMEPATH%\vimfiles /E /I /Y
+REM echo "Installing VIM Config files"
+REM copy /Y .vimrc C:%HOMEPATH%\
+REM xcopy .vim C:%HOMEPATH%\vimfiles /E /I /Y
+
+echo "Installing NVIM Config files"
+xcopy nvim %LOCALAPPDATA%\nvim /E /I /Y
