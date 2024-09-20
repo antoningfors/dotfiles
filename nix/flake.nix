ce85a4f8de6cfd40a02f0fe902ab36969a17676b
Anton Ingfors <anton.ingfors@gmail.com>
2024-09-20 18:15:00 +0200

    new nix flake structure

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,9 +1,13 @@
 {
-	description = "My flake description";
+	description = "Build entire system";
 
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-24.05";
 		nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
 	outputs = { self, nixpkgs, nixpkgs-unstable, ... }: 
@@ -14,9 +18,16 @@
 		pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
 	in {
 		nixosConfigurations = {
+			anton-desktop = lib.nixosSystem {
+				system = "x86_64-linux";
+				modules = [ ./hosts/desktop/configuration.nix ];
+				specialArgs = {
+					inherit pkgs-unstable;
+				};
+			};
 			anton-laptop = lib.nixosSystem {
 				system = "x86_64-linux";
-				modules = [ ./configuration.nix ];
+				modules = [ ./hosts/laptop/configuration.nix ];
 				specialArgs = {
 					inherit pkgs-unstable;
 				};
