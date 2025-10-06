068cac351b2b318277db46f5e8afedaaf17d590a
Anton Ingfors <anton.ingfors@gmail.com>
2025-10-06 19:09:29 +0200

    update

--- .config/sway/status.sh
+++
@@ -1,7 +0,0 @@
-while true; do 
-	volume="Volume: $(amixer get Master | grep -o '[0-9]*%' | head -n 1)"
-	date=$(date +'%Y-%m-%d %T')
-	battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-	echo "$volume | Battery: $battery_capacity% | $date" 
-	sleep 0.2 
-done
